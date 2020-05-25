ab902e6a117197c413aaa36188be39a2a50c125d
radkin <noeldamonmiller@gmail.com>
2020-05-25 13:11:46 -0700

    top_cat2 stripped react scroll out of this project

--- src/AnimatedNavbar.js
+++ src/AnimatedNavbar.js
@@ -6,19 +6,13 @@ import DropdownContainer from "./DropdownContainer"
 import CompanyDropdown from "./DropdownContents/CompanyDropdown"
 import DevelopersDropdown from "./DropdownContents/DevelopersDropdown"
 import ProductsDropdown from "./DropdownContents/ProductsDropdown"
-import Section from "./Components/Section"
 import RevealTopCat from './Components/RevealTopCat';
 import './AnimatedNavbar.css';
-import { Link, animateScroll as scroll } from "react-scroll";
-import logo from "./logo.svg";
 
 const navbarConfig = [
   { title: "Products", dropdown: ProductsDropdown },
   { title: "Developers", dropdown: DevelopersDropdown },
-  { title: "Company", dropdown: CompanyDropdown },
-  { title: "Section1", dropdown: Section },
-  { title: "Section2", dropdown: Section },
-  { title: "Section3", dropdown: Section }
+  { title: "Company", dropdown: CompanyDropdown }
 ]
 
 export default class AnimatedNavbar extends Component {
@@ -59,10 +53,6 @@ export default class AnimatedNavbar extends Component {
       this.props.duration
     )
   }
-
-  scrollToTop = () => {
-      scroll.scrollToTop();
-    };
 
   render() {
     const { duration } = this.props
@@ -121,16 +111,3 @@ export default class AnimatedNavbar extends Component {
     )
   }
 }
-
-
-// <li className="nav-item">
-//   <Link
-//     activeClass="active"
-//     to="section5"
-//     spy={true}
-//     smooth={true}
-//     offset={-70}
-//     duration={500}
-//   >
-//     Section 5
-//   </Link>
--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from "react";
 import "./App.css";
-// import Navbar from "./Components/Navbar";
-import Section from "./Components/Section";
-import dummyText from "./DummyText";
 import AnimatedNavbar from "./AnimatedNavbar"
 import styled from "styled-components"
 
@@ -29,48 +26,9 @@ class App extends Component {
     return (
       <AppContainer>
         <AnimatedNavbar duration={this.state.duration} />
-        <div>
-          <Section
-            title="Section 1"
-            subtitle={dummyText}
-            dark={true}
-            id="section1"
-          />
-          <Section
-            title="Section 2"
-            subtitle={dummyText}
-            dark={false}
-            id="section2"
-          />
-          <Section
-            title="Section 3"
-            subtitle={dummyText}
-            dark={true}
-            id="section3"
-          />
-          <Section
-            title="Section 4"
-            subtitle={dummyText}
-            dark={false}
-            id="section4"
-          />
-          <Section
-            title="Section 5"
-            subtitle={dummyText}
-            dark={true}
-            id="section5"
-          />
-        </div>
       </AppContainer>
     );
   }
 }
 
 export default App;
-
-/*
-<div className="App">
-  <Navbar />
-  <RevealTopCat />
-</div>
-*/
