bc6b910fadbee1d24ccd06166bf7a70084bce063
radkin <noeldamonmiller@gmail.com>
2020-09-09 17:46:15 -0700

    travis updated snapshots after lint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
+  plugins: ["react"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  rules: {
+    "react/jsx-uses-react": "error",
+    "react/jsx-uses-vars": "error"
+  },
   'env': {
     'es6': true,
     'node': true,
@@ -13,5 +23,11 @@ module.exports = {
     'sourceType': 'module',
   },
   'rules': {
+  },
+  settings: {
+    react: {
+      pragma: "React", // Pragma to use, default to "React"
+      version: "detect" // React version
+    }
   },
 };
